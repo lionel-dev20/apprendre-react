9ba963ac02a0f1f6a1eeb670f53399ab3304058f
lionel-dev20 <lioneleda.dev@gmail.com>
2023-04-26 11:45:54 +0100

    installation de tellwing css

--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,6 +1,12 @@
-function Employee () {
+function Employee (props) {
 
-    return <h2>Here is employee</h2>
+    return ( 
+    <>
+    <h2>Here is employee {props.name} </h2>
+    {props.role ? (<p>{props.role}</p>) : (<p>No role</p>)}
+    </>
+
+    ) 
 
 }
 export default Employee;
