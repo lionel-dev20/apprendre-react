27c94de4703dd9b9b30203861ebfa52e906ef27d
lionel-dev20 <lioneleda.dev@gmail.com>
2023-04-27 12:27:51 +0100

    Loop items avec react et uuid

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
 import './index.css';
 import Employee from './components/Employee';
 import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 
 function App() {
-const [role, setRole] = useState('dev');
+  const [role, setRole] = useState('dev')
+  const [employees, setEmployess] = useState (
+    [
+      { 
+        name : "Lionel", 
+      role: "React", 
+      img : "https://i.pinimg.com/564x/74/3e/32/743e32c2cd356eaffe91e5a84443b40a.jpg"
+    },
+      {
+      name : "Evrard", 
+      role: "Flutter", 
+      img : "https://media.techtribune.net/uploads/2022/11/bleach-ichigo-thousand-year-blood-war-anime.jpg"
+    },
+      {
+        id : 3,
+      name : "Gautier", 
+      role: "Dev ops", 
+      img : "https://kanto.legiaodosherois.com.br/w760-h398-gnw-cfill-q95/wp-content/uploads/2021/12/legiao_3CIEFBoT2l4S.jpg.webp"
+    },
+      {
+       
+      name : "Virgil", 
+      role: "Flutter App", 
+      img : "https://i.pinimg.com/564x/37/e8/6d/37e86d8a2b6746f28c3eeddfce2679ec.jpg"}
+    ]
+  )
   const showEmployees = true;
   return (
     <div className="App">
      
       { showEmployees ? (
       <>
-       <input type="text" onChange={(e) => {console.log(e.target.value); setRole(e.target.value)}} />
-        <Employee name="Lionel" role="Developer react js"></Employee>
-        <Employee name ="Evrard" role ="Flutter developer" ></Employee>
-        <Employee role={role}></Employee>
-        <Employee></Employee>
-     
 
+      
+        <div className="flex flex-wrap justify-center">
+         {employees.map( (employee) => {
+           return ( <Employee key = {uuidv4()} name = {employee.name}  role = {employee.role}  img = {employee.img} /> );
+         })}
+        </div>
         </>
     )
     : (
